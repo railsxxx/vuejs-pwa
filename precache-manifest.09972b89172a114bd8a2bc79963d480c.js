62294e353cde2922841444eb0717a9843e0a3f2d
My Name <me@example.com>
2020-12-31 15:58:39 +0100

    deploy

---
+++ precache-manifest.09972b89172a114bd8a2bc79963d480c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c224d0c0c8be6b0dc532",
+    "url": "/vuejs-pwa/css/app.c13b6128.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vuejs-pwa/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "502f3b3851fda2177a581b799ea26424",
+    "url": "/vuejs-pwa/index.html"
+  },
+  {
+    "revision": "c224d0c0c8be6b0dc532",
+    "url": "/vuejs-pwa/js/app.e64bc04f.js"
+  },
+  {
+    "revision": "25b16e8aa3a57d65f3a1",
+    "url": "/vuejs-pwa/js/chunk-vendors.20315bb0.js"
+  },
+  {
+    "revision": "501a854472195e46c1b5a2b4dfba5c59",
+    "url": "/vuejs-pwa/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vuejs-pwa/robots.txt"
+  }
+]);
